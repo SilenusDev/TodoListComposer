06105f11f014cb281bf0645bcdd9430a07fcba5a
Yannick tindy <yannicktindy@gmail.com>
2022-07-13 09:45:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/css/style.css">
+    <title>TODO List</title>
+</head>
+<body>
+<header class="bg-dark">
+    <div class="container ">
+        <h1 class="text-lightgi">TODO List</h1>
+    </div>
+<header>  
+    <ul>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias incidunt nam unde.</li>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias incidunt nam unde.</li>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias incidunt nam unde.</li>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias incidunt nam unde.</li>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias incidunt nam unde.</li>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias incidunt nam unde.</li>  
+    </ul>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
